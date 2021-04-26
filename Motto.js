4540f5b468f78003f685d20ae318c450db1f4eb4
nancyvahling <nanisecond@gmail.com>
2021-04-26 11:03:23 -0600

    Lesson 2 assignment

--- Motto.js
+++ Motto.js
@@ -1,9 +1,28 @@
 alert ("What is your favorite Motto?")
-favorite_motto = input("what is your favorite Motto?" )
-print(" {} is a nice Motto".format(favorite_motto))
 
-var p = prompt("How many times do you want to see your favorite Motto?");
-console.log(createMenu(p));
+var person = prompt("Please enter your favorite Motto");
+let txt;
+if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+} else {
+txt = "{} is a nice Motto". format(favorite_motto);
+}
+alert(txt);   
+
+var i = 1;
+    while(i <= 5) {    
+        console.log(" " + i + "");
+        i++;
+    }
+
+
+
+// alert ("What is your favorite Motto?")
+// favorite_motto = input("what is your favorite Motto?" )
+// print(" {} is a nice Motto".format(favorite_motto))
+
+// var p = prompt("How many times do you want to see your favorite Motto?");
+// console.log(createMenu(p));
 
 // for (var i = 0; i < numberOfMotto; i++) {  // Repeat numberofMotto x times
 
