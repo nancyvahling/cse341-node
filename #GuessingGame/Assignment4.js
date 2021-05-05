7d589a86e4a63220a5b7c94a4ca6368701bd2b68
nancyvahling <nanisecond@gmail.com>
2021-05-05 16:29:34 -0600

    Lesson 4 Project

--- #GuessingGame/Assignment4.js
+++ #GuessingGame/Assignment4.js
@@ -1,35 +1,127 @@
 function submitData() {
-    let Min=parseInt(GuessForm.Min.value);
-    let Max=parseInt(GuessForm.Max.value); 
-}
-if(!Min || !Max || Min >= Max )
-yourGuess()
-{
-    var guess1 = document.getElementById("guess").value;
-    var guess2 = 500;
-    var newGuess = Math.floor(guess2);
-    var n = 0;
-    let randomNumber = Math.floor(Math.random() * 500) + 1;
+    const min_value = document.getElementById("min-number").value;
+    const max_value = document.getElementById("max-number").value;
 
-    if (userGuess < guess1) {
-        yourReturn = "Guess is to low!"
+if (min_value == "" || max_value == "") {
+    updateDom("Your number is not valid! Please enter a value in both inputs.", "red");
+    return;
+}
+
+if (!Number.isInteger(+min_value) || !Number.isInteger(+max_value)) {
+    updateDom("Please input a valid number for both the input boxes above", "red");
+    return;
+}
+
+if (min_value < 1 || min_value > 125) {
+    updateDom("Data is not valid! Please given numbers from 1-125 only.", "red");
+    return;
+}
+if (max_value < 1 || max_value > 125) {
+    updateDom("Data is not valid! Please given numbers from 1-125 only.", "red");
+    return;
+}
+if (min_value >= max_value) {
+    updateDom("Low number should be lesser than the high number... silly! You know this!", "red");
+    return;
+}
+numberGuessGame(parseInt(min_value), parseInt(max_value))
+
+}
+function updateDOM(value, color) {
+    document.getElementById("output").innerHTML = value; //make sure you have a div in your html file with id of output.
+    document.getElementById("output").style.color = color;
+    //update div color also.
+
+}
+function numberGuessGame(min_value, max_value) {
+    let random,randomHintMsg = "I am thinking of a number between " + min_value + " and " + max_value,rangeError=false,
+        randomNumberGenerated = randomNumber(+min_value, +max_value), guessedItems = [],guessedItemsString="Guessed integers - ",
+        attempts = 0;
+    do {
+        if(rangeError){
+            randomHintMsg = random + " is invalid number for given range.";
+            rangeError=false;
+        }else{
+
+            attempts++;
+        if(random && random > randomNumberGenerated){ 
+            randomHintMsg = random + " is high. Please enter a different number.";
+        
+    }
+    if(random && random < randomNumberGenerated){
+        randomHintMsg = random+ " is low. Please try again.";
     }
 }
-    else {
-        if (userGuess > guess1){
-            yourReturn = "Correct Guess - Winner, Winner!"
-        }
+random = prompt(randomHintMsg +".\n Try to guess it!\n Please enter a number between " + min_value + " and " + max_value + ".", '');
+
+if (random == null) {
+    return;
+}else{
+    if(random<min_value || random>max_value){
+        rangeError = true;
+}else{
+    guessedItems.push(random)
     }
-    function numberGuessGame(min, max) {
-        let num = randomNumber(min, max)
-        // declare message variable with something like , "I'm thinking of a number between 'min' and 'max', try to guess it"
-        let guess; // declare guess as empty variable so you can modify it from the loop
-        do {
-            guess = parseInt(prompt(message))
-            // if too low modify message to say that their guess is too low
-            // if too high, modify message to say that they guess is too high
-        } while(..guess is wrong..)
-        // modify message to say congratulations!!!
-        updateDOM(message, "green");
-    }
+}
+if (random == randomNumberGenerated){
+    guessedItems.forEach(function(item,index){
+        let comma = "";
+        if(index < guessedItemsString.length-1){comma=","}
+        guessedItemsString = guessedItemsString+item+comma;
+    })
+    updateDOM("Congratulations, your smart!! " +randomNumberGenerated + " is correct! It only took you " + attempts + " guesses.<br />"+guessedItemsString, "green");
+}
+    } while (random !=randomNumberGenerated)
+}
+function randomNumber(min, max) {
+    let randomNum = Math.random() * (max - min) + min;
+    return Math.round(randomNum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function submitData() {
+//     let Min=parseInt(GuessForm.Min.value);
+//     let Max=parseInt(GuessForm.Max.value); 
+// }
+// if(!Min || !Max || Min >= Max )
+// yourGuess()
+// {
+//     var guess1 = document.getElementById("guess").value;
+//     var guess2 = 100;
+//     var newGuess = Math.floor(guess2);
+//     var n = 0;
+//     let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+//     if (userGuess < guess1) {
+//         yourReturn = "Guess is to low!"
+//     }
+// }
+//     else {
+//         if (userGuess > guess1){
+//             yourReturn = "Correct Guess - Winner, Winner!"
+//         }
+//     }
+//     function numberGuessGame(min, max) {
+//         let num = randomNumber(min, max)
+//         // declare message variable with something like , "I'm thinking of a number between 'min' and 'max', try to guess it"
+//         let guess; // declare guess as empty variable so you can modify it from the loop
+//         do {
+//             guess = parseInt(prompt(message))
+//             // if too low modify message to say that their guess is too low
+//             // if too high, modify message to say that they guess is too high
+//         } while(..guess is wrong..)
+//         // modify message to say congratulations!!!
+//         updateDOM(message, "green");
+//     }
                     
--- function addImage() {     /Form.js
+++ function addImage() {     /Form.js
@@ -3,9 +3,32 @@
 //   let imgElement =  document.createElement("img")
 //       ("src", imageSrc);
 // }
-function addImage() {
-  // let imageSrc = imageForm.imageUrl.value;
-  let imageSrc = document.getElementById("imageUrl").value;
-  let imgElement = document.createElement("img");
-  imgElement.setAttribute("src", imageSrc);
-  document.body.appendChild(imgEleme
+// function addImage() {
+//   // let imageSrc = imageForm.imageUrl.value;
+//   let imageSrc = document.getElementById("imageUrl").value;
+//   let imgElement = document.createElement("img");
+//   imgElement.setAttribute("src", imageSrc);
+//   document.body.appendChild(imgEleme
+
+    function addToList() {
+      // get values from form
+      let product = productForm.productName.value;
+      let price = productForm.productPrice.value;
+  
+      // create table row
+      let tr = document.createElement("tr");
+      // create table cell 1
+      let td1 = document.createElement("td");
+      td1.appendChild(document.createTextNode(product));
+  
+      // create table cell 2
+      let td2 = document.createElement("td");
+      td2.appendChild(document.createTextNode(price));
+  
+      // add cells to table row
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+  
+      document.getElementById("userTable").appendChild(tr);
+      productForm.productName.value = "";
+      productForm.productPrice.value = "";
